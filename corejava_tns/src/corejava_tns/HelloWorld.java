1cdb38af8f5598f38501e8f2802b0f98bf8783e1
akshaya <sreeakshaya153@gmail.com>
2025-08-01 11:46:49 +0530

    first commit

---
+++ corejava_tns/src/corejava_tns/HelloWorld.java
@@ -0,0 +1,10 @@
+package corejava_tns;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
