592954471eb19c5aab7885c67cfd425f59b249b0
akshaya <sreeakshaya153@gmail.com>
2025-08-07 07:37:30 +0530

    first commit

---
+++ corejava/src/org/tnsif/sf/c2tc/basics_java/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.tnsif.sf.c2tc.basics_java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
